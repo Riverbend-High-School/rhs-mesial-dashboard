586c562a921570150c0616f1d3c954857cc4a6ba
Gabriel Hogan <Gabriel.T.Hogan@gmail.com>
2021-09-26 12:48:50 -0400

    Events Coming Soon

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,6 +1,9 @@
 
 <template>
-  <h1>Upcoming Events:</h1>
+  <div class="events_container">
+    <h1>👷‍♂️🚧</h1>
+    <h2>This section is currently under construction!</h2>
+  </div>
 </template>
 
 <script>
@@ -14,4 +17,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+  .events_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    height: 75vh;
+  }
+  h1 {
+    font-size: 12em;
+  }
+  h2 {
+    font-size: 5em;
+  }
+
+
 </style>
