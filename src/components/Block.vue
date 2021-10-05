50403417504828114f591a18739b1da5ae0e946f
Gabriel Hogan <Gabriel.T.Hogan@gmail.com>
2021-10-05 01:19:38 -0400

    Update Block to auto change

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="block_container">
-    <h1 class="block_text">B7 (Second Lunch)</h1>
+    <h1 class="block_text">{{ json.block }}</h1>
   </div>
 </template>
 
 <script>
-
+  import axiosInstance from "../helpers/axiosInstance.ts"
   export default {
     name: 'Block',
-    props: {
-        class : String
+    data() {
+      return {
+        events: [],
+        count_interval: null,
+        loading: true,
+      };
     },
-    mounted () {
+    mounted: function() {
+      this.loading = true;
+      this.getBlock();
+      this.count_interval = setInterval(() => {
+        this.getEvents();
+      }, 60000);
+    },
+    methods: {
+      getEvents() {
+        axiosInstance
+          .get("/schedule/")
+          .then((response) => {
+            this.events = response.data;
+            this.loading = false;
+          })
+          .catch((error) => console.error(error));
+      },
     }
   }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
