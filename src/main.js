07f395c74366e399b0f1d89cd74d45494d07957d
Samuel Rembisz <sjrembisz07@gmail.com>
2022-01-19 23:42:44 -0500

    Re-enabled sentry

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import * as Sentry from "@sentry/vue";
+import * as Sentry from "@sentry/vue";
 
-// Sentry.init({
-//     App,
-//     dsn: process.env.VUE_APP_SENTRYDSN,
-//     tracesSampleRate: 1.0,
-//   });
+Sentry.init({
+    App,
+    dsn: process.env.VUE_APP_SENTRYDSN,
+    tracesSampleRate: 1.0,
+  });
 
 createApp(App).mount('#app')
 
-// App.mixin(Sentry.createTracingMixins({ trackComponents: true }));
-// Sentry.attachErrorHandler(App, { logErrors: true });
+App.mixin(Sentry.createTracingMixins({ trackComponents: true }));
+Sentry.attachErrorHandler(App, { logErrors: true });
